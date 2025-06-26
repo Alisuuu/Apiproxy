f0dc54687c278dc007ffb63a4aaeb3cb99b927a6
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-06-26 16:41:06 -0300

    Update and rename api/serie.js to api/serie/[slug].js

--- api/serie/[slug].js
+++ api/serie/[slug].js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const { slug } = req.query;
+  const slug = req.query.slug;
   const url = `https://superflixapi.lat/serie/${slug}`;
 
   try {
     const response = await fetch(url);
-    const data = await response.text();
+    const html = await response.text();
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'text/html');
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).send('Erro ao buscar série.');
+    res.status(200).send(html);
+  } catch (error) {
+    res.status(500).send('Erro ao carregar série.');
   }
 };
